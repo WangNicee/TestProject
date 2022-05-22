c8e53a7c9007392ae4a3a88f4a168018ea2d7892
WangNicee <47596356+WangNicee@users.noreply.github.com>
2022-05-22 17:04:32 +0800

    Update Main.java
    手动修改了

--- TestImage/src/com/company/Main.java
+++ TestImage/src/com/company/Main.java
@@ -5,5 +5,6 @@ package com.company;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("我要push代码");
+        System.out.println("远程库的版本比本地库的更新，所以要pull拉取到远程库的最新代码");
     }
 }
